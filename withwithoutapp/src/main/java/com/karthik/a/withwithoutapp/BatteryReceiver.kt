e7ec7c78442ef9b12706fc745d950cd67bb5115b
karthik <karthiklearning58@gmail.com>
2024-07-22 21:27:04 +0530

    static

---
+++ withwithoutapp/src/main/java/com/karthik/a/withwithoutapp/BatteryReceiver.kt
@@ -0,0 +1,40 @@
+package com.karthik.a.withwithoutapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.BatteryManager
+import androidx.core.app.NotificationCompat
+
+class BatteryReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+        if (level == 61) {
+            showNotification(context)
+        }
+    }
+
+    private fun showNotification(context: Context) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationId = 1
+
+        val notificationBuilder = NotificationCompat.Builder(context, "battery_channel")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Battery Level")
+            .setContentText("Battery level is at 80%")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        val channel = NotificationChannel(
+            "battery_channel",
+            "Battery Notifications",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        notificationManager.createNotificationChannel(channel)
+
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+}
--- withwithoutapp/src/main/java/com/karthik/a/withwithoutapp/MainActivity.kt
+++ withwithoutapp/src/main/java/com/karthik/a/withwithoutapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.karthik.a.withwithoutapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var batteryReceiver: BatteryReceiver
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted:Boolean ->
+        if(!isGranted){
+            Toast.makeText(this, "Notification will not be shown", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +33,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        askNotificationPermission()
+        registerBroadcast()
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if(hasNotificationPermission()){
+
+            }else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)){
+                showLocationDeniedPermission()
+            } else{
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun registerBroadcast(){
+        batteryReceiver = BatteryReceiver()
+        registerReceiver(batteryReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+    }
+
+    private fun showLocationDeniedPermission(){}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(batteryReceiver)
     }
 }
